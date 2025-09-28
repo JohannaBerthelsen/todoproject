6d4e1d23ab2584a6eb9ec5dcf97ba364e5711d74
JohannaBerthelsen <johannalberthelsen@gmail.com>
2025-09-28 15:26:03 +0200

    refactor: clean up in app file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class App {
-  protected readonly title = signal('todoproject');
+export class AppComponent {
+
 }
